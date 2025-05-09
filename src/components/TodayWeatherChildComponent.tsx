4f56e1e04e7acbe204fcfe4fe8ed8c79bc6a8e20
AnNguyen <nhatan20032@gmail.com>
2025-05-09 14:28:13 +0700

    feat: add key on div

--- src/components/TodayWeatherChildComponent.tsx
+++ src/components/TodayWeatherChildComponent.tsx
@@ -3,8 +3,8 @@ const TodayWeatherChildComponent = () => {
   const { forecasts } = useTodayForecast();
   return (
     <div className="grid grid-cols-6 divide-x-1 divide-gray-700">
-      {forecasts.map((item) => (
-        <div className="text-center justify-center">
+      {forecasts.map((item, index) => (
+        <div key={index} className="text-center justify-center">
           <div className="text-gray-500 font-bold">{item.dt_txt}</div>
           <img
             src={`https://openweathermap.org/img/wn/${item.icon}@2x.png`}
